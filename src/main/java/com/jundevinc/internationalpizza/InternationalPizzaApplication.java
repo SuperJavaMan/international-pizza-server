fc31f1b2fa336e5a047563a9b6d45134ddaaeb9b
Oleg <cpabox@gmail.com>
2019-12-18 20:04:46 +0300

    Initial commit

---
+++ src/main/java/com/jundevinc/internationalpizza/InternationalPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.jundevinc.internationalpizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalPizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternationalPizzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jundevinc/internationalpizza/InternationalPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jundevinc.internationalpizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalPizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
